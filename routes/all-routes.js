a8179f4ac90db2b1b657374efcde4814cf568850
bobimel2nd <31457563+bobimel2nd@users.noreply.github.com>
2018-03-05 21:45:42 -0800

    Version 0.0.10

--- routes/all-routes.js
+++ routes/all-routes.js
@@ -136,23 +136,89 @@ module.exports = function(app) {
     });
   })
 
+  // Save Changes from update display to Database
+  app.post("/Save/:id", function(req, res) {
+    MyRecipeID = req.params.id
+    MyRecipes = req.body.Recipes
+    MyIngredients = req.body.Ingredients
+    MyDirections = req.body.Directions
+
+    // Update Recipe
+    db.Recipes.findOrCreate({
+        where: { id:  MyRecipeID}
+      }).then (Recipe => {
+          Recipe.updateAttributes({
+            title: MyRecipes.title,
+            description: MyRecipes.description,
+            picture: MyRecipes.picture,
+            keyWords: MyRecipes.keyWords
+          })
+      })
+
+      // Update/Delete Ingredients
+      req.Ingredients.forEach((ingredient, index) => {
+        db.Ingredients.findOrCreate({
+            where: {  recipeID: MyRecipeID,
+                      itm:  index+1}
+          }).then (Ingredient => {
+              Recipe.updateAttributes({
+                recipeID: MyRecipeID,
+                itm:  index+1,
+                qty: MyIngredients.qty,
+                unitID: MyIngredients.unitID,
+                ingredient: MyIngredients.ingredient
+              })
+          })
+      })
+      db.Ingredients.destroy({
+          where: {  recipeID: MyRecipeID,
+                    itm:  {[Op.gt]: MyIngredients.length}}
+      })
+
+      // Update/Delete Ingredients
+      req.Directions.forEach((direction, index) => {
+        db.Directions.findOrCreate({
+            where: {  recipeID: MyRecipeID,
+                      step:  index+1}
+          }).then (Direction => {
+              Direction.updateAttributes({
+                recipeID: MyRecipeID,
+                step:  index+1,
+                direction: MyDirections.direction
+              })
+          })
+      })
+      db.Directions.destroy({
+          where: {  recipeID: MyRecipeID,
+                    step:  {[Op.gt]: MyDirections.length}}
+      })
+  })
+
   // Update Database from update display
-  app.post("/image", function(req, res) {
+  app.post("/image/:id", function(req, res) {
     if (!req.files)
       return res.status(400).send('No files were uploaded.');
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let sampleFile = req.files.sampleFile;
-    console.log(sampleFile)
     if (["image/jpeg","image/png","image/bmp","image/gif"].indexOf(sampleFile.mimetype) < 0) {
       return res.status(400).send('Invalid File type ' + sampleFile.mimetype);
     }
 
+    var posIdx = sampleFile.name.lastIndexOf('.');
+    var fileName = sampleFile.name.substr(0, posIdx) + Date.now() + sampleFile.name.substr(posIdx)
+
     // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv("./public/images/" + sampleFile.name, function(err) {
-      if (err)
-        return res.status(500).send(err);
-      res.send("./public/images/" + sampleFile.name);
+    sampleFile.mv("./public/images/" + fileName, function(err) {
+      if (err) return res.status(500).send(err);
+      db.Recipes.update({
+          picture: fileName
+        },{
+          where: {id: req.params.id}
+        }).then(dbRecipe =>  {
+          console.log("Updated Picutre for id=" + dbRecipe.id)
+          return res.send(200);
+        });
+      });
     });
-  })
 }
