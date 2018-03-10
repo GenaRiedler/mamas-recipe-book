6bec686ac5cd1fbf4215380d6442876cfbdef0ae
bobimel2nd <31457563+bobimel2nd@users.noreply.github.com>
2018-03-09 18:19:03 -0800

    Version 0.3.3

--- models/index.js
+++ models/index.js
@@ -14,15 +14,24 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+// fs
+//   .readdirSync(__dirname)
+//   .filter(function(file) {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(function(file) {
+//     var model = sequelize['import'](path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+    var model = sequelize['import'](path.join(__dirname, 'units.js'));
     db[model.name] = model;
-  });
+    var model = sequelize['import'](path.join(__dirname, 'recipes.js'));
+    db[model.name] = model;
+    var model = sequelize['import'](path.join(__dirname, 'ingredients'));
+    db[model.name] = model;
+    var model = sequelize['import'](path.join(__dirname, 'directions'));
+    db[model.name] = model;
 
 Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
